adb89296302247377b58648c9241aa1df475d78f
hebi26 <veleine.lucas@gmail.com>
2017-11-27 16:50:10 +0100

    fire ok

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var left = -100;
 var transition = false;
-
+var counter = 0;
+var posX;
+var posY;
 //=================================
 //====================================
 //function canvas animation
@@ -10,23 +12,22 @@ var transition = false;
 //=======================================
 $(document).ready(function(){
 //===========================================
-
+// iCI SCRIPT DANIM SI CANVAS
 //===========================================
 
     var baseWidth  = $(window).width();
-    console.log(baseWidth);
+    // console.log(baseWidth);
 
     $(window).mousemove(function(e){
-        var posX = (e.pageX);
-        var posY = (e.pageY);
+        posX = (e.pageX);
+        posY = (e.pageY);
         console.log(posX, posY);
         calcPosX(posX, baseWidth);
-
     });
 
     function calcPosX(posX, baseWidth) {
         var posXpercent = (posX * 100) / baseWidth;
-        console.log(posXpercent);
+        // console.log(posXpercent);
 
         if(posXpercent < 5){
             // calcLeft();
@@ -65,4 +66,40 @@ $(document).ready(function(){
         }, 800);
     }
 
+    $(".box").click(function () {
+
+        console.log(posX, posY);
+        counter++;
+        var boxid = $(this).attr('id');
+        fire(boxid);
+    });
+
+    function fire(boxid) {
+
+        $("#soundFire").get(0).play();
+
+        $('#' + boxid).append('<div class="bullet" id="bullet' + counter + '"></div>');
+        var bX = 0;
+        var bY = 0;
+        $("#bullet" + counter).css({
+            left: posX - bX + "px",
+            top: posY - bY + "px"
+        });
+        $('.bullet').animate({height: '0px', width: '0px'}, "slow");
+
+        $('.target').click(function () {
+            var targetid = $(this).attr('id');
+            setTimeout(function () {
+                explode(targetid);
+            }, 1000);
+
+        });
+    }
+    function explode(targetid) {
+        $("#soundExplode").get(0).play();
+        $("#"+targetid).css({backgroundColor: "black",
+                            boxShadow: "0 0 5px red, 0 0 25px orangered, 0 0 35px gold, 0 0 40px yellow",
+                            transition: 500});
+        $("#"+targetid).fadeOut(1500);
+    }
 });
