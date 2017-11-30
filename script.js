62b47001c0510e984b8fd8c5f9e767782325f800
hebi26 <veleine.lucas@gmail.com>
2017-11-30 09:55:03 +0100

    shipsound

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var left = -100;
 var transition = false;
+var touched = false;
+var anim1 = false;
+var anim2 = false;
+var anim3 = false;
 var counter = 0;
 var posX;
 var posY;
@@ -23,7 +27,8 @@ $(document).ready(function(){
         posX = (e.pageX);                       //on recup position curseur X Y
         posY = (e.pageY);
         // console.log(posX, posY);
-        calcPosX(posX, baseWidth);              // on appele la fonction calcul
+        calcPosX(posX, baseWidth); // on appele la fonction calcul
+
     });
 
     $(".box").click(function () {
@@ -32,10 +37,6 @@ $(document).ready(function(){
         counter++;
         var boxid = $(this).attr('id');   //ON RECUP ATTR ID
         fire(boxid);
-    });
-
-    $('#box3').hover(function() {
-        timerShip3();
     });
 
         //==============FUNCTION CALCUL % POS===================================//
@@ -52,6 +53,7 @@ $(document).ready(function(){
             // calcLeft();
             moveRight(posXpercent);                         //on appele moveright
         }
+
     }
 //=========FUNCTION MOVMENT LEFT================//
 
@@ -62,6 +64,7 @@ $(document).ready(function(){
             $(".mainbox").css({
                 left: left + "%" });
             setInterval();
+            checkAnim();
         }
     }
 //=========FUNCTION MOVMENT RIGHT================//
@@ -73,6 +76,7 @@ $(document).ready(function(){
             $(".mainbox").css({
                 left: left + "%" });
             setInterval();
+            checkAnim();
         }
     }
 //===========FUNCTION SET INTERVAL POUR L'ENCHAINEMENT DE SLIDE=========//
@@ -98,18 +102,24 @@ $(document).ready(function(){
             left: posX - bX + "px",
             top: posY - bY + "px"
         });
-        $('.bullet').animate({height: '0px', width: '0px'}, "slow");
+        $('.bullet').animate({height: '0px', width: '0px'}, 500);
+        if($('.bullet').length === 10){
+            setTimeout(function(){
+            $('.bullet').remove()}, 550);
+        }
 
-        $('.target, .ship').click(function (e) {                    //QUAND ON CLIQUE SUR UNE CIBLE
+
+        $('.target, .ship').click(function (e) {  //QUAND ON CLIQUE SUR UNE CIBLE
+            touched=true;
             var targetid = $(this).attr('id');
-            var boxlayoutid = $(this).parent().attr('id');
+            var boxid = $(this).parent().attr('id');
             i = parseInt(/\d/.exec(targetid));
             var nposX = (e.pageX);
             var nposY = (e.pageY);
 
 
             setTimeout(function () {                        //ON APPEL EFUNCTION EXPLODE APRES 1S
-                explode(targetid, boxlayoutid, nposX, nposY, i);
+                explode(targetid, boxid, nposX, nposY);
             }, 1000);
 
         });
@@ -118,36 +128,78 @@ $(document).ready(function(){
     //==========PARALLAX=========================
     //=========FUNCTION EXPLODE==========================
 
-    function explode(targetid, boxlayoutid, nposX, nposY, i) {
-        var player = document.getElementById("soundExplode");
-        player.pause();
-        player.currentTime = 0;
-        player.play();
+    function explode(targetid, boxid, nposX, nposY) {
+        if (touched) {
+            var player = document.getElementById("soundExplode");
+            player.pause();
+            player.currentTime = 0;
+            player.play();
+            touched = false;
+        }
 
-        $("#"+boxlayoutid).append('<img class="touch" src="img/explode1.png">');
-        var bX = 150;
-        var bY = 150;
-        $(".touch").css({
-            left: nposX - bX + "px",
-            top: nposY - bY + "px"
-        });
-        $('.touch').fadeOut(500);
-        $("#"+targetid).fadeOut(1000);
+            $("#" + boxid).append('<img class="touch" src="img/explode1.png">');
+            var bX = 150;
+            var bY = 150;
+            $(".touch").css({
+                left: nposX - bX + "px",
+                top: nposY - bY + "px"
+            });
 
-        setTimeout(function () {                        //ON APPEL EFUNCTION EXPLODE APRES 500mS
-            $("#infos"+i).fadeIn(2000);
-        }, 500);
+
+            $('.touch').fadeOut(500);
+            $("#" + targetid).fadeOut(500);
+            if ($('.touch').length === 10) {
+                setTimeout(function () {
+                    $('.touch').remove()
+                }, 550);
+            }
+
+            // setTimeout(function () {                        //ON APPEL EFUNCTION EXPLODE APRES 500mS
+            $("#infos" + i).fadeIn(500);
+            // }, 500);
+
     }
 
-    //========FUNCTION ANIM TEXTE=====//
+    //==============FUNCTION CHECK POSITION POUR LANCER ANIM 1, 2 ou 3
+
+    function checkAnim() {
+        if (left === -200 && !anim3) {
+            anim3 = true;
+            timerShip3();
+        }
+        // else if (left === -100) {
+        //
+        // }
+        // else {
+        //     anim1 = false;
+        //     anim2 = false;
+        //     anim3 = true;
+        // }
+    }
+
+
     //========FUNCTION ANIM SAPCESHIP
 
-    function timerShip3(){
-        setTimeout(function(){
-        $(".ship3").animate({width: "300px", height: "300px"}, 500)}, 500);
+    function timerShip3() {
 
-        setTimeout(function(){
-            $(".ship2").animate({width: "200px", height: "200px"}, 500)}, 1000);
+        var player = document.getElementById("soundShip");
+
+        setTimeout(function () {
+            $(".ship3").animate({width: "300px", height: "300px"},{ start: function(){
+                player.pause();
+                player.currentTime = 0;
+                player.play();
+
+            }}, 1000)
+        }, 500);
+
+        setTimeout(function () {
+            $(".ship2").animate({width: "200px", height: "200px"},{ start: function() {
+                player.pause();
+                player.currentTime = 0;
+                player.play();
+            }}, 1000)
+        }, 1000);
     }
 
 });
